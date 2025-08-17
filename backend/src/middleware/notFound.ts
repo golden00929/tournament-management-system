d884c04d1e55516d6aa087c12722163d245f1753
golden00929 <golden00929@gmail.com>
2025-08-17 23:37:49 +0700

    Delete backend directory

--- backend/src/middleware/notFound.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-
-export const notFound = (req: Request, res: Response) => {
-  return res.status(404).json({
-    success: false,
-    message: `요청한 경로 ${req.originalUrl}를 찾을 수 없습니다.`,
-    error: 'NOT_FOUND'
-  });
-};
